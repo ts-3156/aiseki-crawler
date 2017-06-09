1b1c9b24967d5191a604ff2bc05717535985cf4b
ts-3156 <ts_3156@yahoo.co.jp>
2017-06-09 01:53:11 +0000

    wrap in `module Crawl`

--- crawl.rb
+++ crawl.rb
@@ -2,62 +2,64 @@
 
 require 'anemone'
 
-url = 'http://oriental-lounge.com/'
-places = %w(
-  sapporo
-  sendai
-  shinjuku
-  shibuya
-  machida
-  nagoya
-  kyoto
-  shinsaibashi
-  kobe
-  hiroshima
-  fukuoka
-  kumamoto
-  kagoshima
-  okinawa
-)
-
-class Cache
-  class << self
-    def time2key(time)
-      time.strftime("%Y%m%d%H%M") # yyyymmddhhmm
-    end
-
-    def dir
-      "#{Dir.pwd}/.cache"
-    end
-
-    def file(time)
-      "#{dir}/#{time2key(time)}"
-    end
-
-    def exists?(time)
-      FileTest.exist?(file(time))
-    end
-
-    def read(time)
-      File.read(file(time))
-    end
-
-    def write(time, html)
-      FileUtils.mkdir_p(dir) unless FileTest.exist?(dir)
-      File.write(file(time), html)
-    end
-
-    def fetch(time)
-      if block_given?
-        if exists?(time)
-          read(time)
+module Crawl
+  URL = 'http://oriental-lounge.com/'
+  PLACES = %w(
+    sapporo
+    sendai
+    shinjuku
+    shibuya
+    machida
+    nagoya
+    kyoto
+    shinsaibashi
+    kobe
+    hiroshima
+    fukuoka
+    kumamoto
+    kagoshima
+    okinawa
+  )
+  
+  class Cache
+    class << self
+      def time2key(time)
+        time.strftime("%Y%m%d%H%M") # yyyymmddhhmm
+      end
+  
+      def dir
+        "#{Dir.pwd}/.cache"
+      end
+  
+      def file(time)
+        "#{dir}/#{time2key(time)}"
+      end
+  
+      def exists?(time)
+        FileTest.exist?(file(time))
+      end
+  
+      def read(time)
+        File.read(file(time))
+      end
+  
+      def write(time, html)
+        FileUtils.mkdir_p(dir) unless FileTest.exist?(dir)
+        File.write(file(time), html)
+      end
+  
+      def fetch(time)
+        if block_given?
+          if exists?(time)
+            read(time)
+          else
+            result = yield
+            write(time, result)
+            result
+          end
         else
-          result = yield
-          write(time, result)
-          result
+          read(time)
         end
-      else
-        read(time)
       end
     end
   end
@@ -82,12 +84,12 @@ def extract(str)
 end
 
 if $0 == __FILE__
-  html = Cache.fetch(Time.now) { fetch(url).to_html }
+  html = Crawl::Cache.fetch(Time.now) { fetch(Crawl::URL).to_html }
   doc = Nokogiri::HTML(html)
-  counts = places.map { |p| parse(doc, "//a[@id='box_#{p}']") }.map { |s| extract(s) }
+  counts = Crawl::PLACES.map { |p| parse(doc, "//a[@id='box_#{p}']") }.map { |s| extract(s) }
 
   print Time.now.to_s + ' '
-  puts places.map.with_index { |p, i| "#{p} #{counts[i][0]} #{counts[i][1]}" }.join(', ')
+  puts Crawl::PLACES.map.with_index { |p, i| "#{p} #{counts[i][0]} #{counts[i][1]}" }.join(', ')
 end
 
 
