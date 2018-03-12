4adb971b2a5a8fba85d0cad2958c197b6c18c352
ts-3156 <ts_3156@yahoo.co.jp>
2018-03-13 06:28:03 +0900

    Add json gem

--- convert.rb
+++ convert.rb
@@ -4,12 +4,17 @@ require 'time'
 require 'json'
 
 if $0 == __FILE__
+  #
+  # ruby convert.rb log/cron.log shinjuku man >docs/shinjuku_man.json
+  #
+
+  log = ARGV[0]
   name, gender = ARGV[1..2]
   regexp = /(?<time>\d+-\d+-\d+ \d+:\d+:\d+ \+0000).+#{name} (?<man>\d+) (?<woman>\d+)/
 
   json = []
 
-  File.read('log/cron.log').each_line do |line|
+  File.read(log).each_line do |line|
     next unless (m = line.match regexp)
     json << [Time.parse(m[:time]).to_i * 1000, m[gender.to_sym].to_i]
   end
